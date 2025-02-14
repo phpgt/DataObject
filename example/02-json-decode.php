7bfc7a729d88a1dde14c230c4a274c192c3642f7
Greg Bowler <greg.bowler@g105b.com>
2025-02-14 11:18:17 +0000

    wip: update examples

---
+++ example/02-json-decode.php
@@ -0,0 +1,15 @@
+<?php
+use Gt\DataObject\DataObjectBuilder;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+$jsonString = '{"name":"Cody","colour":"orange","food":["biscuits","mushrooms","corn on the cob"]}';
+
+$builder = new DataObjectBuilder();
+$obj = $builder->fromObject(json_decode($jsonString));
+
+echo "Hello, ",
+	$obj->getString("name"),
+	"! Your favourite food is ",
+	$obj->getArray("food")[0],
+	PHP_EOL;
